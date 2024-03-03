75ef4cbb4efd68075872e80b82d1ad20a24926af
Mikhail Kuznetsov <jonik.sid1@gmail.com>
2024-03-03 22:17:40 +0300

    feat: extracted data from pdf

--- commercial-converter-api/controllers/pdf-excel.js
+++ commercial-converter-api/controllers/pdf-excel.js
@@ -16,6 +16,73 @@ const extractTextFromPDF = async (pdfFileData) => {
   }
 };
 
+// Метод для объединения строк с "ШТ" где был перенос
+const mergeLinesWithSHTRows = (lines) => {
+  const mergedLines = [];
+  let buffer = '';
+
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i];
+
+    if (/^\d+\s[A-Za-zА-Яа-я]{1,}\S{2}/.test(line)) {
+      if (buffer !== '') {
+        mergedLines.push(buffer.trim());
+        buffer = '';
+      }
+      buffer += line;
+    } else if (line.includes('ШТ')) {
+      buffer += ` ${line}`;
+    } else if (line.includes('Система')) {
+      mergedLines.push(line.trim()); // Добавляем строку с "Система" в итоговый массив
+    } else {
+      buffer += `${line} `;
+    }
+  }
+
+  if (buffer !== '') {
+    mergedLines.push(buffer.trim());
+  }
+
+  return mergedLines;
+};
+
+// Функция для извлечения данных о системах из итогового массива
+const extractDataFromMergedLines = (mergedLines) => {
+  const systems = [];
+
+  mergedLines.forEach((line) => {
+    if (line.includes('Система')) {
+      const systemName = line.split(',')[0].trim(); // Получаем название системы
+      systems.push(systemName);
+    }
+
+    // Находим наименование и цену для каждой системы
+    const match = line.match(/^\d+\s[A-Za-zА-Яа-я]{1,}\S{2}/);
+    if (match) {
+      const orderNumberMatch = line.match(/^\d+/);
+      const orderNumber = orderNumberMatch ? orderNumberMatch[0] : null;
+      const quantityMatch = line.match(/(\d+)\sШТ/);
+      const quantity = quantityMatch ? quantityMatch[1] : null;
+      const priceMatch = line.match(/ШТ\s(\d+\s\d+,\d+)/) || line.match(/ШТ\s(\d+,\d+)/);
+      const price = priceMatch ? priceMatch[1] : null;
+
+      // Находим позицию начала и конца строки с наименованием
+      const itemNameStartIndex = line.indexOf(orderNumber) + orderNumber.length;
+      const itemNameEndIndex = line.indexOf(' ШТ');
+      const itemName = line.slice(itemNameStartIndex, itemNameEndIndex - 1).trim();
+
+      systems.push({
+        orderNumber,
+        itemName,
+        quantity,
+        price,
+      });
+    }
+  });
+
+  return systems;
+};
+
 // POST запрос для извлечения текста из PDF и сохранения в XLSX
 module.exports.pdfToXLSX = async (req, res, next) => {
   try {
@@ -36,13 +103,28 @@ module.exports.pdfToXLSX = async (req, res, next) => {
       // Итерируем по массиву и фильтруем нужные строки
       extractedText.forEach((pageText) => {
         const lines = pageText.split('\n');
-        lines.forEach((line) => {
-          data.push(line);
-        });
+        const mergedLines = mergeLinesWithSHTRows(lines);
+        const filteredLines = mergedLines.filter((line) => /^\d+\s[A-Za-zА-Яа-я]{1,}\S{2}/.test(line) || /^Система/.test(line));
+        data.push(...filteredLines);
       });
       // Вставляем данные в таблицу Excel
       data.forEach((row) => {
-        worksheet.addRow(row.split('\n'));
+        worksheet.addRow([row]);
+      });
+
+      // Извлекаем данные о системах из итогового массива
+      const systemsData = extractDataFromMergedLines(data);
+
+      // Выводим данные о системах в консоль
+      console.log('Данные по системам:');
+      systemsData.forEach((system) => {
+        // Не работает сейчас, systemName нет у system, название лежит вне system
+        // Для такого элемента массива выводится undefind по всем свойствам
+        // console.log('Название системы:', system.systemName);
+        console.log('Наименование:', system.itemName);
+        console.log('Количество:', system.quantity);
+        console.log('Цена:', system.price);
+        console.log('---------------------');
       });
     } else {
       throw new Error('Извлеченный текст не является массивом');
