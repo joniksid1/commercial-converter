1d6edab715addf14943087fdea18cff91573e4a1
Mikhail Kuznetsov <jonik.sid1@gmail.com>
2024-03-26 20:04:31 +0300

    feat: add file drag over

--- commercial-converter-frontend/src/components/app.jsx
+++ commercial-converter-frontend/src/components/app.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function FileUploadForm() {
@@ -6,6 +6,44 @@ function FileUploadForm() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
   const [fileName, setFileName] = useState('');
+  const [dragOver, setDragOver] = useState(false);
+
+  useEffect(() => {
+    const handleDocumentDragOver = (event) => {
+      event.preventDefault();
+      setDragOver(true);
+    };
+
+    const handleDocumentDragLeave = (event) => {
+      event.preventDefault();
+      setDragOver(false);
+    };
+
+    const handleDocumentDrop = (event) => {
+      event.preventDefault();
+      setDragOver(false);
+      const droppedFile = event.dataTransfer.files[0];
+      if (droppedFile && droppedFile.name.endsWith('.pdf')) {
+        setError(null);
+        setFile(droppedFile);
+        setFileName(droppedFile.name);
+      } else {
+        setError('Пожалуйста, перетащите файл в формате PDF');
+        setFile(null);
+        setFileName('');
+      }
+    };
+
+    document.addEventListener('dragover', handleDocumentDragOver);
+    document.addEventListener('dragleave', handleDocumentDragLeave);
+    document.addEventListener('drop', handleDocumentDrop);
+
+    return () => {
+      document.removeEventListener('dragover', handleDocumentDragOver);
+      document.removeEventListener('dragleave', handleDocumentDragLeave);
+      document.removeEventListener('drop', handleDocumentDrop);
+    };
+  }, []);
 
   const handleFileChange = (event) => {
     const selectedFile = event.target.files[0];
@@ -14,7 +52,6 @@ function FileUploadForm() {
       setFile(selectedFile);
       setFileName(selectedFile.name);
     } else {
-      // Обработка случая, когда выбран неверный файл
       setError('Пожалуйста, выберите файл в формате PDF');
       setFile(null);
       setFileName('');
@@ -34,10 +71,6 @@ function FileUploadForm() {
         responseType: 'blob',
       });
 
-      // const response = await axios.post('http://localhost:3000/convert', formData, {
-      //   responseType: 'blob',
-      // });
-
       const url = window.URL.createObjectURL(new Blob([response.data]));
       const link = document.createElement('a');
       link.href = url;
@@ -56,7 +89,7 @@ function FileUploadForm() {
 
         const errorMessage = JSON.parse(blobText).message;
         console.log('Сообщение от сервера:', errorMessage);
-        setError(errorMessage || 'Произошла ошибка при загрузке файла. Пожалуйста, попробуйте еще раз или обратитесь к администратору..');
+        setError(errorMessage || 'Произошла ошибка при загрузке файла. Пожалуйста, попробуйте еще раз или обратитесь к администратору.');
       } else {
         console.log('Произошла ошибка:', error.message);
         setError('Произошла ошибка при запросе на сервер. Пожалуйста, попробуйте ещё раз или обратитесь к администратору.');
@@ -66,14 +99,15 @@ function FileUploadForm() {
   };
 
   return (
-    <main className='main'>
-      <h2 className='main__header'>Загрузите PDF файл</h2>
+    <main className={`main ${dragOver ? 'drag-over' : ''}`}>
+      <div className={`drag-over-overlay ${dragOver ? 'visible' : ''}`}></div> {/* Элемент с зелёной тенью */}
+      <h2 className='main__header'>Загрузите или перетащите PDF файл</h2>
       {error && <div className='error'>{error}</div>}
       <form className='form' method="post" encType="multipart/form-data" onSubmit={handleSubmit}>
         <label className='form__input-file'>
           <input type="file" name="file" accept=".pdf" onChange={handleFileChange} />
-          <span className='form__input-file-btn'>Выберите файл</span>
-          <span className='form__input-file-text'>{fileName ? fileName : 'Максимум 10мб'}</span>
+          <span className='form__input-file-btn'>Загрузить PDF файл</span>
+          <span className='form__input-file-text'>{fileName ? fileName : 'Максимум 10 МБ'}</span>
         </label>
         <button className='form__button' type="submit" disabled={!file || isLoading}>
           {isLoading ? 'Загрузка...' : 'Сформировать ТКП в XLSX'}
@@ -81,6 +115,7 @@ function FileUploadForm() {
       </form>
     </main>
   );
+
 }
 
 export default FileUploadForm;
