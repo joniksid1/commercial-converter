c621d47cf6279da5fc42da40722cedab90774b04
Mikhail Kuznetsov <jonik.sid1@gmail.com>
2024-05-01 15:47:44 +0300

    feat: add Math round in format-system-name

--- commercial-converter-api/utils/format-system-name.js
+++ commercial-converter-api/utils/format-system-name.js
@@ -5,7 +5,7 @@ function formatSystemName(originalName) {
   // Преобразуем формат чисел в скобках
   formattedName = formattedName.replace(/\(([\d.]+)\s*т\.м3\/час;\s*([\d.]+)\s*Па\)/, (match, volume, pressure) => {
     // Преобразуем объем из тысяч метров кубических в метры кубические
-    const volumeInM3 = parseFloat(volume.replace(',', '.')) * 1000;
+    const volumeInM3 = Math.round(parseFloat(volume.replace(',', '.')) * 1000);
     // Округляем давление, если есть дробная часть
     const roundedPressure = Math.round(parseFloat(pressure.replace(',', '.')));
     return `(L=${volumeInM3} м3/час; Рс=${roundedPressure} Па)`;
